fc4889fc6127666f6c1611c166548aece1f84238
Brandon kwamou <brandonkwamou@gmail.com>
2023-05-25 06:24:30 +0200

    daily quote completed

--- Day_32/birthday_wisher/main.py
+++ Day_32/birthday_wisher/main.py
@@ -1,12 +1,13 @@
-import smtplib
+##################### Extra Hard Starting Project ######################
 
-my_email = "brandonkwamou@gmail.com"
-my_password = "gkgypsgasccijypk"
+# 1. Update the birthdays.csv
 
-connection = smtplib.SMTP("smtp.gmail.com", 587)
-connection.ehlo()
-connection.starttls()
-connection.login(user=my_email, password=my_password)
-connection.sendmail(from_addr=my_email,
-                    to_addrs="juniorkwamou1@yahoo.com", msg="Hello")
-connection.close()
+# 2. Check if today matches a birthday in the birthdays.csv
+
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+
+# 4. Send the letter generated in step 3 to that person's email address.
+
+
+
+
