24a7d500b7137e8d4ed6491d19d26aef05677810
Brandon kwamou <brandonkwamou@gmail.com>
2023-06-02 06:50:50 +0200

    Day 35 hourly cast

--- Day_35/main.py
+++ Day_35/main.py
@@ -1,12 +1,26 @@
 import requests
 
-params = {
-    "appid": "cacdd43ad779e6bd48011907a8c327fd",
-    "lat": "51",
-    "lon": "-0.9"
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+api_key = "cacdd43ad779e6bd48011907a8c327fd"
+
+weather_params = {
+    "lat": 51.507351,
+    "lon": -0.127758,
+    "appid": api_key,
+    "exclude": "current,minutely,daily"
 }
 
 response = requests.get(
-    f"https://api.openweathermap.org/data/2.5/onecall?", params=params)
+    OWM_Endpoint, params=weather_params)
+weather_data = response.json()
+weather_slice = weather_data["hourly"][:12]
 
-print(response.json())
+will_rain = False
+
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    print("bring an umbrella")
