e5bfa5de088348bab3359c5f6c89a53d185063d0
Brandon kwamou <brandonkwamou@gmail.com>
2023-06-04 22:44:42 +0200

    Day 35 Completed

--- Day_35/main.py
+++ Day_35/main.py
@@ -1,11 +1,14 @@
 import requests
+from twilio.rest import Client
 
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
 api_key = "cacdd43ad779e6bd48011907a8c327fd"
+account_sid = "AC99482de55ddc023fe19665ccdb5e4e49"
+auth_token = "b64e37af264e187cdb7f292a86d27581"
 
 weather_params = {
-    "lat":  48.856614,
-    "lon":  2.3522219,
+    "lat":  43.769562,
+    "lon":  11.255814,
     "appid": api_key,
     "exclude": "current,minutely,daily"
 }
@@ -23,4 +26,9 @@ for hour_data in weather_slice:
         will_rain = True
 
 if will_rain:
-    print("bring an umbrella")
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="It's going to rain today. Remember to bring an ☂️",
+        from_="+13203788034",
+        to="YOUR PHONE NUMBER"
+    )
