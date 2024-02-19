ed769d623cd9e667eb3747bc10f3e2524bba3a5e
Brandon Kwamou <brandonkwamou@gmail.com>
2024-02-19 15:45:21 +0100

    Day_1 challenges

---
+++ Day_1/main.py
@@ -0,0 +1,16 @@
+# challenge 1 :
+# print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.\n\n2. Knead the dough for 10 minutes.\n\n3. Add 3g of Salt.\n\n4. Leave to rise for 2 hours.\n\n5. Bake at 200 degrees C for 30 minutes.")
+
+# Challenge 2:
+
+# Fix the code below 👇
+
+# print(Day 1 - String Manipulation")
+# print("String Concatenation is done with the "+" sign.")
+#     print('e.g.print("Hello " + "world")')
+# print(("New lines can be created with a backslash and n.")
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the \"+\" sign.")
+print('e.g.print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
